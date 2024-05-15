12b4827efce1315cfb544530c27e02efb8df1202
ZsTech^ <wellingtonzsescola@gmail.com>
2024-05-15 10:59:04 -0300

    teste

---
+++ src/main/java/br/com/inf3fm/charityconnect/CharityConnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.inf3fm.charityconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inf3fm/charityconnect/CharityConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inf3fm.charityconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
