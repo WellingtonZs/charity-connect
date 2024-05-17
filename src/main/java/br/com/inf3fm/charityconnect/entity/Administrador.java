d06a2e09a13fd1a46fcd6bfcb4894bf1d1969af3
Leandro Niches <126569905+lecoTV@users.noreply.github.com>
2024-05-17 10:51:33 -0300

    add entity adm
    adicionando entity admnistrado

---
+++ src/main/java/br/com/inf3fm/charityconnect/entity/Administrador.java
@@ -0,0 +1,119 @@
+package br.com.inf3fm.charityconnect.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Administrador")
+public class Administrador {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String cpf;
+	private String email;
+	private String nome;
+	private String sobrenome;
+	private String senha;
+	private String uf;
+	private LocalDateTime dataNasc;
+	private String cep;
+	private LocalDateTime dataCadastro;
+	private String telefone;
+	private String nivelAcesso;
+	private Byte[] foto;
+	private String statusAdmin;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public LocalDateTime getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(LocalDateTime dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getNivelAcesso() {
+		return nivelAcesso;
+	}
+	public void setNivelAcesso(String nivelAcesso) {
+		this.nivelAcesso = nivelAcesso;
+	}
+	public Byte[] getFoto() {
+		return foto;
+	}
+	public void setFoto(Byte[] foto) {
+		this.foto = foto;
+	}
+	public String getStatusAdmin() {
+		return statusAdmin;
+	}
+	public void setStatusAdmin(String statusAdmin) {
+		this.statusAdmin = statusAdmin;
+	}
+	
+	
+	
+
+}
---
+++ src/main/resources/bd_CharityConnect_INF3FM.sql
@@ -0,0 +1,132 @@
+USE MASTER IF EXISTS(
+SELECT * FROM SYS.databases 
+WHERE NAME = 'bd_CharityConnect_INF3FM')
+
+DROP DATABASE bd_CharityConnect_INF3FM
+GO
+CREATE DATABASE bd_CharityConnect_INF3FM
+GO
+
+USE bd_CharityConnect_INF3FM
+
+--Criação das tabelas:
+
+--Administrador:
+CREATE TABLE Administrador
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	cpf				VARCHAR(11)		NOT NULL,
+	email			VARCHAR(100)	UNIQUE	 NOT NULL,
+	nome			VARCHAR(100)	NOT NULL,
+	sobrenome		VARCHAR(100)	NOT NULL,
+	senha			VARCHAR(100)	NOT NULL,
+	uf				CHAR(2)			NOT NULL,
+	dataNasc		DATETIME		NOT NULL,
+	cep				CHAR(7)			NOT NULL,
+	dataCadastro	DATETIME		NOT NULL,
+	telefone		VARCHAR(20)		NOT NULL,
+	nivelAcesso		CHAR(10)		NULL, --ADMIN ou USER
+	foto			VARBINARY(MAX)	NULL,
+	statusAdmin		VARCHAR(20)		NOT NULL,
+)
+
+--ONG:
+CREATE TABLE ONG
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nome			VARCHAR(100)	NOT NULL,
+	email			VARCHAR(100)	UNIQUE	NOT NULL,
+	senha			VARCHAR(100)	NOT NULL,
+	telefone		VARCHAR(20)		NOT NULL,
+	descAtuacao		VARCHAR(300)	NOT NULL,
+	foto			VARBINARY(MAX)	NULL,
+	cep				CHAR(7)			NOT NULL,
+	dataCadastro	DATETIME		NOT NULL,
+	sobreNos		VARCHAR(200)	NOT NULL,
+	uf				CHAR(2)			NOT NULL,
+	cnpj			VARCHAR(14)		NOT NULL,
+	statusONG		VARCHAR(20)		NOT NULL,
+)
+
+--AprovacaoONG
+CREATE TABLE AprovacaoONG
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	dataAprovacao	DATETIME		NOT NULL,
+	statusAprovacao	CHAR(20)		NOT NULL,
+	admin_id		BIGINT			NOT NULL,
+	ong_id			BIGINT			NOT NULL,
+
+	FOREIGN KEY (admin_id)	REFERENCES Administrador(id),
+	FOREIGN KEY (ong_id)	REFERENCES ONG(id)
+)
+
+--ReprovacaoONG
+CREATE TABLE ReprovacaoONG
+(
+	id				 BIGINT				NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	dataReprovacao	 DATETIME			NOT NULL,
+	statusReprovacao CHAR(20)			NOT NULL,
+	admin_id		 BIGINT				NOT NULL,
+	ong_id			 BIGINT				NOT NULL,
+
+	FOREIGN KEY (admin_id)	REFERENCES Administrador(id),
+	FOREIGN KEY (ong_id)	REFERENCES ONG(id)
+)
+
+--CategoriaRoupas:
+CREATE TABLE Categoria
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	genero			VARCHAR(100)	NOT NULL,
+)
+
+--Inserts das Tabelas:
+
+--ONG:
+INSERT INTO ONG (nome, email, senha, telefone, descAtuacao, foto, cep, dataCadastro, sobreNos, uf, cnpj, statusONG) 
+VALUES ('zezo', 'aaa@aaa.com', '111', '11952303304', 'somos uma ong teste', null, '1231234', '2021-01-08T00:00:00', 'aaaa', 'rj', '11111111111111', 'ativo')
+
+--Administrador:
+INSERT INTO Administrador (cpf, email, nome, sobrenome, senha, uf, dataNasc, cep, dataCadastro, telefone, nivelAcesso, foto, statusAdmin) 
+VALUES ('11111111111', 'AA@AA.COM', 'aaa', 'aaa', '11212', 'pe', '2021-01-08T00:00:00', '1234567', '2021-01-08T00:00:00', '11952303304', 'admin', null, 'ativo')
+
+--Aprovacao:
+INSERT INTO AprovacaoONG (dataAprovacao, statusAprovacao, ong_id, admin_id)
+VALUES('2021-01-08T00:00:00', 'aprovado', 1, 1)
+
+--Reprovacao:
+INSERT INTO ReprovacaoONG (dataReprovacao, statusReprovacao, ong_id, admin_id)
+VALUES('2021-01-08T00:00:00', 'reprovado', 1, 1)
+
+--Categoria:
+INSERT INTO Categoria (genero) 
+VALUES ('DINHEIRO OU ROUPA')
+
+--Selects:
+
+--ONG
+SELECT * FROM ONG
+--Administrador
+SELECT * FROM Administrador
+--CategoriaRoupas
+SELECT * FROM Categoria
+--ContatoSuporte
+SELECT * FROM AprovacaoONG
+--ReprovaçãoONG
+SELECT * FROM ReprovacaoONG
+
+
+--Alterações feita com base na atualização em andamento do MER/DER, não mexer
+
+
+
+--Protótipo SQL Charity Connect by ZsTech^
+--https://github.com/WellingtonZs
+
+--__________       ___________           .__       /\  
+--\____    /  _____\__    ___/___   ____ |  |__   /  \ 
+--  /     /  /  ___/ |    |_/ __ \_/ ___\|  |  \  \/\/ 
+-- /     /_  \___ \  |    |\  ___/\  \___|   Y  \      
+--/_______ \/____  > |____| \___  >\___  >___|  /      
+--        \/     \/             \/     \/     \/       
