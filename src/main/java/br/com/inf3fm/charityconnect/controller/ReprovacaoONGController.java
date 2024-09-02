d7e02c5905ca26460a036c0ad7602c4f103d4f60
Leandro Niches <126569905+lecoTV@users.noreply.github.com>
2024-09-02 11:05:00 -0300

    ultimos updates
    .

--- src/main/java/br/com/inf3fm/charityconnect/controller/ReprovacaoONGController.java
+++ src/main/java/br/com/inf3fm/charityconnect/controller/ReprovacaoONGController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,16 @@ public class ReprovacaoONGController {
 		ReprovacaoONG _reprovacaoong = reprovacaoongService.create(reprovacaoong);
 		
 		return new ResponseEntity<ReprovacaoONG>(_reprovacaoong, HttpStatus.OK);
+	}
+	
+	@PutMapping("update/{id}")
+	public ResponseEntity<ReprovacaoONG> update(
+			@PathVariable long id) {
+		
+		ReprovacaoONG _reprovacaoong = reprovacaoongService.update(id);
+		
+		return new ResponseEntity<ReprovacaoONG>(
+				_reprovacaoong, HttpStatus.OK);
 	}
 
 }
--- src/main/java/br/com/inf3fm/charityconnect/service/ReprovacaoONGService.java
+++ src/main/java/br/com/inf3fm/charityconnect/service/ReprovacaoONGService.java
@@ -2,6 +2,7 @@ package br.com.inf3fm.charityconnect.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -37,6 +38,20 @@ public class ReprovacaoONGService {
 		reprovacaoong.setStatusReprovacao("REPROVADA");
 		
 		return reprovacaoongRepository.save(reprovacaoong);
+	}
+	
+	@Transactional
+	public ReprovacaoONG update(long id) {
+		Optional<ReprovacaoONG> _reprovacaoong = reprovacaoongRepository.findById(id);
+		
+		if (_reprovacaoong.isPresent()) {
+			ReprovacaoONG reprovacaoongAtualizada = _reprovacaoong.get();
+			reprovacaoongAtualizada.setDataReprovacao(LocalDateTime.now());
+			reprovacaoongAtualizada.setStatusReprovacao("REPROVADA");
+			
+			return reprovacaoongRepository.save(reprovacaoongAtualizada);
+		}
+		return null;
 	}
 
 }
